184a9c052ad31c32a7b531fb073b362d509a634e
Yiankaso <yiankasosemeyian@gmail.com>
2022-10-19 14:47:02 -0700

    My commit task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - prints _putchar followed by a new line
+ * main - prints _putcher followed by a new line
  *
  * Return: Always 0
  */
@@ -19,4 +19,3 @@ int main(void)
 
 	return (0);
 }
-
