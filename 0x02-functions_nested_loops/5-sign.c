892f5bb2efabd422c68b76ba8a49758e2e50e56a
Yiankaso <yiankasosemeyian@gmail.com>
2022-10-19 18:28:16 -0700

    My commit task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,8 +5,8 @@
  * @n: The number of which the sign will be printed
  *
  * Return: 1 if the number is greater than zero
- * Return: 0 if the number is zero
- * Return: -1 if the number is less than zero
+ *         0 if the number is zero
+ *        -1 if the number is less than zero
  */
 int print_sign(int n)
 {
