823dae7f6af924b38483d3fca4e0b251f81e183c
Yiankaso <yiankasosemeyian@gmail.com>
2022-10-22 19:24:48 -0700

    My commit task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply 2 numbers
+ * @a: first number
+ * @b: second number
+ * Return: results of multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
